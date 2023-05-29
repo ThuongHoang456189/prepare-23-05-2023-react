397f8dbdec17f2f4c736a20a530a3edc5fb8d389
Thuong Hoang <ngocanh654189@gmail.com>
2023-05-29 08:15:04 +0700

    Oidc API Fetch

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -6,12 +6,21 @@ import {
     useOidcUser
 } from "@axa-fr/react-oidc";
 import Layout from "../Layout";
-import CustomDiv from "./CustomDiv";
+
+fetch('https://localhost:44348/api/sras/admin-hello', {
+    method: 'GET'
+}).then(response => response.json())
+    .then(data => {console.log(data)})
+    .catch(error => {
+        console.log(error);
+    });
 
 const Admin = () => {
     const {oidcUser, oidcUserLoadingState} = useOidcUser();
     const {idToken, idTokenPayload} = useOidcIdToken();
     const {accessToken, accessTokenPayload} = useOidcAccessToken();
+
+
     return (
         <OidcSecure>
             <Layout>
@@ -26,13 +35,6 @@ const Admin = () => {
                 <p>{JSON.stringify(accessToken)}</p>
                 <p>Payload</p>
                 <p>{JSON.stringify(accessTokenPayload)}</p>
-                <CustomDiv fetch={fetch('https://localhost:44348/api/sras/admin-hello', {
-                    method: 'GET'
-                }).then(response => response.json())
-                    .then(data => {console.log(data)})
-                    .catch(error => {
-                        console.log(error);
-                    })}/>
             </Layout>
         </OidcSecure>
     );
